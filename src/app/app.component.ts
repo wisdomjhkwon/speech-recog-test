91ac1013d15d284633dfc8d1d33f243b30516dea
wisdomjhkwon <wisdomjhkwon@gmail.com>
2017-07-24 12:05:08 +0900

    애니메이션 예제 보고 그대로 되도록 구현했당

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 import { HomePage } from '../pages/home/home';
+import { WebApiPage } from '../pages/web-api/web-api';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = WebApiPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,28 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WebApiPage } from '../pages/web-api/web-api';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    WebApiPage
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    WebApiPage
   ],
   providers: [
     StatusBar,
